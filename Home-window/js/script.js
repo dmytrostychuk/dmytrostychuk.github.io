8f072b5d5262fb1f6ffbc05cb58a872bc12144fe
Dmitriy Stychuk <72120575+dmytrostychuk@users.noreply.github.com>
2021-10-09 10:17:36 +0300

    Add files via upload

---
+++ Home-window/js/script.js
@@ -0,0 +1,35 @@
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+});
+
+// Меню бургер
+const burgerBth = document.querySelector('.burger-bth');
+const menuNav = document.querySelector('.menu');
+if (burgerBth) {
+	burgerBth.addEventListener("click", function (e) {
+		menuNav.classList.toggle('menu-active');
+		burgerBth.classList.toggle('bth-active');
+	});
+};
+
+
+
+$('.slider').slick({
+	arrows: false,
+	variableWidth: true,
+	slidesToScroll: 2,
+	infinite: false,
+	draggable: false,
+	
+  });
