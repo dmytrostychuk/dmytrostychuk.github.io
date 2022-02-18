1babb8831dcdf027e1eab1f369433b2dee7e281a
Dmitriy Stychuk <72120575+dmytrostychuk@users.noreply.github.com>
2022-02-18 21:59:17 +0200

    Add files via upload

--- Focus/js/script.js
+++ Focus/js/script.js
@@ -2,46 +2,55 @@
 const iconMenu = document.querySelector('.burger-bth');
 const menuBody = document.querySelector('.nav');
 
-
 if (iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
 		document.body.classList.toggle('lock');
 		iconMenu.classList.toggle('active');
 		menuBody.classList.toggle('active');
-		
 	});
 }
 
+const navContainer = document.querySelector('.nav__container');
 
+navContainer.addEventListener('click',
+	function(event) {
+		console.log('click', event.target == navContainer)
+		if (navContainer.parentElement.classList.contains('active') && 
+				event.target == navContainer) {
+			iconMenu.classList.toggle('active');
+			menuBody.classList.toggle('active');
+		}
+	}
+);
 
 // Модальне вікно
 let isInit = false;
-function openModal(){
-	document.body.classList.toggle('lock');
- 	let elModal  = document.querySelector('.modal');
-    if(isInit==false) {
-    isInit = true;
-    document.querySelector('.modal__close').addEventListener('click',
-        function(event) {
-			document.body.classList.toggle('lock');
-           event.preventDefault();
-           elModal.classList.toggle('active');
-        }
-    );
-  }
 
+function openModal() {
+ 	let elModal  = document.querySelector('.modal');
+	
+	 	if (menuBody.classList.contains('active')) {
+			document.body.classList.toggle('lock');
+			iconMenu.classList.toggle('active');
+			menuBody.classList.toggle('active');
+		}
+
+		document.body.classList.toggle('lock');
+
+    if (isInit == false) {
+			isInit = true;
+
+			document.querySelector('.modal__close').addEventListener('click',
+					function(event) {
+						event.preventDefault();
+						document.body.classList.toggle('lock');
+						elModal.classList.toggle('active');
+					}
+			);
+  }
 
   elModal.classList.toggle('active');
 }
-
-/*
-Specials initialization
-1) написати анімацію в цсс таймлайн
-2) знайти елементи в джс в змінну
-3) видалити модифікатор в теперішнього елемента і добавити наступному (анімацію)
-4) приховати теперішній головний елемент (картинка) і показати наступний
-5) це все має бути в сет таймауті на 5сек
-*/
 
 function initializeSpecials() {
 	const tabsEl = document.getElementsByClassName('special__list')[0].children;
@@ -89,28 +98,15 @@ const swiper = new Swiper('.slider-first__swiper', {
 	  prevEl: '.swiper-button-prev',
 	},
 
-
-
 	// Відступ між слайдами
 	spaceBetween: 28,
 
 	//Кількість слайдів для показу
 	slidesPerView: 3,
 
-		// autoplay: {
-		// 	delay: 2000,
-		// 	stopOnLastSlide: true,
-		// 	disableOnInteraction: false
-		// },
-
-	spead: 700,
-
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
-			// Безкінечність
-			// loop: true,
-
 			scrollbar: {
 				el: '.slider-first__scrollbar',
 				draggable: true
@@ -132,7 +128,6 @@ const swiper = new Swiper('.slider-first__swiper', {
 		1472: {
 			slidesPerView: 3,
 			autoplay:false,
-
 		}
 	}
   });
@@ -151,18 +146,9 @@ const swiper = new Swiper('.slider-first__swiper', {
 	// Відступ між слайдами
 	spaceBetween: 25,
 
-	// autoplay: {
-	// 	delay: 1500,
-	// 	stopOnLastSlide: true,
-	// 	disableOnInteraction: false
-	// },
-
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
-
-			// Безкінечність
-			// loop: true,
 
 			scrollbar: {
 				el: '.slider-second__scrollbar',
@@ -173,79 +159,53 @@ const swiper = new Swiper('.slider-first__swiper', {
 		768: {
 			slidesPerView: 2,
 			autoplay:false,
-
 		},
 
 		1200: {
 			slidesPerView: 3,
 			autoplay:false,
-
 		},
 
 		1472: {
 			slidesPerView: 4,
 			autoplay:false,
-
 		}
 	}
   });
 
   const swiper3 = new Swiper('.slider-third',{
 
-	// Навігаційні стрілки
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	  },
-
-	// Кількість слайдів для показу
-	slidesPerView: 1,
-
-	// autoplay: {
-	// 	delay: 1500,
-	// 	stopOnLastSlide: true,
-	// 	disableOnInteraction: false
-	// },
-
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-
-			// Безкінечність
-			// loop: true,
-
-			scrollbar: {
-				el: '.slider-third__scrollbar',
-				draggable: true
+		// Навігаційні стрілки
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
 			},
-		},
 
-		768: {
-			autoplay:false,
+		// Кількість слайдів для показу
+		slidesPerView: 1,
 
-		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				scrollbar: {
+					el: '.slider-third__scrollbar',
+					draggable: true
+				},
+			},
 
-		1200: {
-			autoplay:false,
+			768: {
+				autoplay:false,
+			},
 
-		},
+			1200: {
+				autoplay:false,
+			},
 
-		1472: {
-			autoplay:false,
+			1472: {
+				autoplay:false,
 
+			}
 		}
-	}
-	
-	// Безкінечність
-	// loop: true,
-
-	// autoplay: {
-	// 	delay: 3000,
-	// 	stopOnLastSlide: true,
-	// 	disableOnInteraction: false
-	// },
-
-	// spead: 700,
   });
 
 
