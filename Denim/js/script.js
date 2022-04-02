63616dae6372eb61960028c9ebb28a26ab0b9ba3
Dmitriy Stychuk <72120575+dmytrostychuk@users.noreply.github.com>
2022-04-02 17:51:32 +0300

    Add files via upload

--- Denim/js/script.js
+++ Denim/js/script.js
@@ -8,88 +8,74 @@ if (burgerBth) {
 	});
 }
 
-
-const swiper = new Swiper(".swiper", {
-	// Навігаційні стрілки
+const swiper = new Swiper('.gallery-top', {
+	slidesPerView: 1,
 	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	breakpoints: {
+		768: {
+			allowTouchMove: false,
+		},
+		1366: {
+			allowTouchMove: true,
+		}
+	},
+	thumbs: {
+		swiper: {
+			el: '.gallery-thumbs',
+			slidesPerView: 4,
+			slidesPerView: 'auto',
+			allowTouchMove: false,
+		}
 	},
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 	$(".product__items").slice(0, 4).show();
-	$(".view__all").on("click", function(e){
-	  e.preventDefault();
-	  $(".product__items:hidden").slice(0, 4).slideDown();
-	  if($(".product__items:hidden").length == 0) {
-		$(".view__all").text("No Content").addClass("no-сontent");
-	  }
-	});
-	
-  })
-
-	$(document).ready(function(){
-		$(".product__items--bootom").slice(0, 4).show();
-		$(".view__all--bottom").on("click", function(e){
-			e.preventDefault();
-			$(".product__items--bootom:hidden").slice(0, 4).slideDown();
-			if($(".product__items--bootom:hidden").length == 0) {
-			$(".view__all--bottom").text("No Content").addClass("no-сontent");
-			}
-		});
-		
-		})
-
-
-$(document).ready(function(){
-	$(".woman__card").slice(0, 8).show();
-	$(".card-load").on("click", function(e){
-	  e.preventDefault();
-	  $(".woman__card:hidden").slice(0, 2).slideDown();
-	  if($(".woman__card:hidden").length == 0) {
-		$(".card-load").text("No Content").addClass("no-сontent");
-	  }
-	});
-	
-  })
-
-  
-
-
-//   Form-------------
-  checked=false;
-function checkedAll (form) {
-	var aa= document.getElementById('form');
-	 if (checked == false){
-           checked = false
-          }
-	for (var i =0; i < aa.elements.length; i++) {
-	 aa.elements[i].checked = checked;
-	}
-}
-
-  //slider
-  $(document).ready(function(){
-	$('.slider').slick({
-		responsive:[
-			{
-				breakpoint: 768,
-				settings:{
-					arrows: true
-				}
-				
-			},{
-				breakpoint: 1366,
-				settings:{
-					arrows: false
-				},
-				
-			}
-		]
-		
+	$(".view__all").on("click", function (e) {
+		e.preventDefault();
+		$(".product__items:hidden").slice(0, 4).slideDown();
+		if ($(".product__items:hidden").length == 0) {
+			$(".view__all").text("No Content").addClass("no-сontent");
+		}
 	});
 })
+
+$(document).ready(function () {
+	$(".product__items--bootom").slice(0, 4).show();
+	$(".view__all--bottom").on("click", function (e) {
+		e.preventDefault();
+		$(".product__items--bootom:hidden").slice(0, 4).slideDown();
+		if ($(".product__items--bootom:hidden").length == 0) {
+			$(".view__all--bottom").text("No Content").addClass("no-сontent");
+		}
+	});
+})
+
+
+$(document).ready(function () {
+	$(".woman__card").slice(0, 8).show();
+	$(".card-load").on("click", function (e) {
+		e.preventDefault();
+		$(".woman__card:hidden").slice(0, 2).slideDown();
+		if ($(".woman__card:hidden").length == 0) {
+			$(".card-load").text("No Content").addClass("no-сontent");
+		}
+	});
+})
+
+checked = false;
+function checkedAll(form) {
+	var aa = document.getElementById('form');
+	if (checked == false) {
+		checked = false
+	}
+	for (var i = 0; i < aa.elements.length; i++) {
+		aa.elements[i].checked = checked;
+	}
+}
 
 $("#slider-range").slider({
 	range: true,
@@ -97,15 +83,13 @@ $("#slider-range").slider({
 	max: 300,
 	values: [0, 300],
 	step: 1,
-	slide: function(event, ui) {
-	  $("#rub-left").text(ui.values[0] + '$'); // текст левого span
-	  $("#rub-right").text(ui.values[1] + '$'); // текст правого span
+	slide: function (event, ui) {
+		$("#rub-left").text(ui.values[0] + '$'); // текст левого span
+		$("#rub-right").text(ui.values[1] + '$'); // текст правого span
 	}
-  });
-  // задать начальный текст левого span
-  $("#rub-left").text($("#slider-range").slider("values", 0) + '$');
-  // задать начальный текст правого span
-  $("#rub-right").text($("#slider-range").slider("values", 1) + '$');
-
-  //   -------------Form
+});
+// задать начальный текст левого span
+$("#rub-left").text($("#slider-range").slider("values", 0) + '$');
+// задать начальный текст правого span
+$("#rub-right").text($("#slider-range").slider("values", 1) + '$');
 
